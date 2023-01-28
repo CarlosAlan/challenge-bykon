f7eda7238e630f4d6b4ec350f2bbd9abc1eac90b
Carlos alan <carlos_alan_garcia@hotmail.com>
2023-01-28 10:43:06 -0600

    Validacion de campos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,6 +52,7 @@ export class AppComponent implements AfterViewInit {
           
           for(let i = 1; i < allTextLines.length; i++){
             let company:string[] = allTextLines[i].split(',');
+
             let oCompany: Company = {
               line: i+1,
               id: company[0],
@@ -64,10 +65,111 @@ export class AppComponent implements AfterViewInit {
             };
 
             this.companies.push(oCompany);
-
           }
 
+          this.validateCompanies();
+
       }
+  }
+
+  validateCompanies(){
+    for(let i = 0; i < this.companies.length; i++){
+      let errores = new Array<string>;
+      let aux:Company = this.companies[i];
+
+      if(this.wrongNumber(aux.id)){
+        errores.push("Campo ID no es un numerico");
+      }
+      if(this.isNotAlphanumeric(aux.empresa)){
+        errores.push("Campo EMPRESA solo puede contener letras, números y espacios con máximo de 50 caracteres");
+      }
+      if(this.isNotRfcValid(aux.rfc)){
+        errores.push("Campo RFC invalido");
+      }
+      if(this.isNotValidEmail(aux.email)){
+        errores.push("Campo EMAIL invalido");
+      }
+      if(this.isNotValidPhone(aux.telefono)){
+        errores.push("Campo TELEFONO invalido");
+      }
+      if(this.isNotBoolean(aux.extranjera)){
+        errores.push("Campo EXTRANJERA invalido");
+      }
+
+      aux.errores = errores;
+
+      this.companies[i] = aux;
+    }
+
+
+  }
+
+  wrongNumber(str:string){
+    if(!isNaN(+str)){
+      if(Number(str) > 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  isNotAlphanumeric(str:string){
+    if(str===null || str===""){
+      return false
+    }
+    var regex = /^[a-zA-Z0-9 ]*$/;
+    if(!regex.test(str) || str.length > 50){
+      return true;
+    }
+    return false;
+  }
+
+  isNotRfcValid(str:string){
+    if(str===null || str===""){
+      return true
+    }
+    var regex = /^[A-Z0-9]*$/;
+    if(!regex.test(str) || str.length > 13){
+      return true;
+    }
+    return false;
+  }
+
+  isNotValidEmail(str:string){
+    if(str===null || str===""){
+      return true
+    }
+    var regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if(!regex.test(str)){
+      return true;
+    }
+    return false;
+  }
+
+  isNotValidPhone(str:string){
+    if(str===null || str===""){
+      return false
+    }
+    var regex = /^[\d]/;
+    if(!regex.test(str) || str.length > 9){
+      return true;
+    }
+    return false;
+  }
+
+  isNotBoolean(str:string){
+    if(str===null || str===""){
+      return true
+    }
+    if(str === "true"
+      || str === "1"
+      || str === "false"
+      || str === "0"
+      || str === "verdadero"
+      || str ===  "falso"){
+      return false;
+      }
+      return true;
   }
 
 }
@@ -80,5 +182,5 @@ interface Company  {
   email: string;
   telefono: string;
   extranjera: string;
-  errores: string;
+  errores: string[];
 }
