459d8774b4de41ec3c4e4e802a3f3106a97802ee
Carlos alan <carlos_alan_garcia@hotmail.com>
2023-01-28 00:26:44 -0600

    avance back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,72 @@ export class AppComponent implements AfterViewInit {
           .body.style.backgroundColor = '#e5e8e8';
   }
 
-  title = 'challenge-bykon';
+  error = "";
+  csvFile: File | null = null;
+  companies = [];
+
+  // On file Select
+  onChange(event) {
+    this.csvFile = event.target.files[0];
+    this.error = "";
+  }
+
+  validateCsv(){
+    if( this.csvFile == null){
+      this.error = "No se ha seleccionado un archivo a validar";
+      return;
+    }
+
+    let ext = this.csvFile.name.split('.').pop();
+
+    if(ext != "csv"){
+      this.error = "Por favor, selecciona un archivo con extension .csv";
+      return;
+    }
+
+    this.readCsv();
+
+  }
+
+  readCsv(){
+    let reader: FileReader = new FileReader();
+         reader.readAsText(this.csvFile);
+         
+         reader.onload = (e) => {
+          let csv: any = reader.result;
+          let allTextLines = [];
+          allTextLines = csv.split(/\r\n|\r|\n/, -1);
+          
+          
+          for(let i = 1; i < allTextLines.length; i++){
+            let company:string[] = allTextLines[i].split(',');
+            let oCompany: Company = {
+              line: i+1,
+              id: company[0],
+              empresa: company[1],
+              rfc: company[2],
+              email: company[3],
+              telefono : company[4],
+              extranjera: company[5],
+              errores: null
+            };
+
+            this.companies.push(oCompany);
+
+          }
+
+      }
+  }
+
+}
+
+interface Company  {
+  line: number;
+  id: string;
+  empresa: string;
+  rfc: string;
+  email: string;
+  telefono: string;
+  extranjera: string;
+  errores: string;
 }
