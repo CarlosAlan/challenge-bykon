7c1bfd936adce7afa63131a61fb9783ecfe8c102
Carlos alan <carlos_alan_garcia@hotmail.com>
2023-01-27 19:05:52 -0600

    ejercicio front-end

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  
+  constructor(private elementRef: ElementRef) {}
+  ngAfterViewInit() {
+      this.elementRef.nativeElement.ownerDocument
+          .body.style.backgroundColor = '#e5e8e8';
+  }
+
+  title = 'challenge-bykon';
+}
